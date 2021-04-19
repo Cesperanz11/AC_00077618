07d829ec88df4f8faa246abc6f761930768d73a7
Cesperanz11 <00077618@uca.edu.sv>
2021-04-18 18:39:03 -0600

    Arreglando errores y agregando imagenes

--- Tarea Laboratorio 2/Tarea1.asm
+++ Tarea Laboratorio 2/Tarea1.asm
@@ -13,15 +13,15 @@
 	;Haciendo direccionamiento directo o absoluto (de una celda de memoria a un registro o viceversa)
 	;Mandar info de 200h a AX
 
-	mov	AX, [200h]
+	mov	AL, [200h]
 
 	;Haciendo direccionamiento indirecto por registro 
 	;(de una celda de memoria[BX] a un registro o viceversa, ocupando BX como el lugar que tiene guardada la direccion de memoria a ocupar)
 	;Mandar info de 201h a CX
 
-	mov	BX, [201h]	;Preparamos el registro base con la info necesitada
+	mov	BX, 201h	;Preparamos el registro base con la info necesitada
 
-	mov	CX, [BX]
+	mov	CL, [BX]
 
 	;Haciendo direccionamiento indirecto base + indice	
 	;(de una celda de memoria[BX+DI/SI] a un registro o viceversa, 
@@ -29,7 +29,7 @@
 	;Mandar info de 202h a DX
 	mov	SI, 1h  ;Teniendo que BX tiene a 201h, le daremos 1h a DI para que sumen 202h
 
-	mov	DX, [BX + SI]
+	mov	DL, [BX + SI]
 
 	;Haciendo direccionamiento relativo por registro
 	;(de una celda de memoria[BX+ memoria] a un registro o viceversa, 
