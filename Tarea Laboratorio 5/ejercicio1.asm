ce9d84af8b7afb970eb20e05038213d18515e5f4
Cesperanz11 <00077618@uca.edu.sv>
2021-06-13 14:15:14 -0600

    Creando carpeta de laboratorio 5

---
+++ Tarea Laboratorio 5/ejercicio1.asm
@@ -0,0 +1,17 @@
+	org	100h
+
+
+	;Aqui va todo el codigo principal
+	section	.text
+
+	;Funcion main
+
+	
+
+
+
+	;Aqui van todas las declaraciones de variables
+	section	.data
+
+
+	;Aqui van todas las funciones
