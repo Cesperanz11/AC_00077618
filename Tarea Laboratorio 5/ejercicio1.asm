9adbf4397b1febd09272f586fe244aec449f8c6e
Cesperanz11 <00077618@uca.edu.sv>
2021-06-13 21:49:39 -0600

    Arreglando errores del codigo

--- Tarea Laboratorio 5/ejercicio1.asm
+++ Tarea Laboratorio 5/ejercicio1.asm
@@ -11,34 +11,46 @@ org	100h
 	xor	DX, DX
 	xor	SI, SI
 
-	
+	;Aqui llamaremos al modo texto con las especificaciones deseadas
+	call modotexto ; solo debemos de llamarlo una vez, sino se borrara toda la pantalla
 
+	;ESCRIBIENDO PRIMER NOMBRE
+
+	;Seteando los contadores en 0
 	MOV	SI, 0
 	MOV	DI, 0d
 
-	;Escribiendo primer nombre
-
-	call modotexto
 	call movercursor1
-	call escribirsegundoapellido
+	call escribirprimer_nombre
 
+	;ESCRIBIENDO SEGUNDO NOMBRE
+
+	;Seteando los contadores en 0
 	MOV	SI, 0
 	MOV	DI, 0d
 
-	;Escribiendo segundo nombre
-
-	call modotexto
 	call movercursor2
-	call escribirsegundonombre
+	call escribirsegundo_nombre
 
+	;ESCRIBIENDO PRIMER APELLIDO
+
+	;Seteando los contadores en 0
 	MOV	SI, 0
 	MOV	DI, 0d
 
-	;Escribiendo primer apellido
+	call movercursor3
+	call escribirprimer_apellido
 
-	call modotexto
-	call movercursor1
-	call escribirprimerapellido
+	;ESCRIBIENDO SEGUNDO APELLIDO
+
+	;Seteando los contadores en 0
+	MOV	SI, 0
+	MOV	DI, 0d
+
+	call movercursor4
+	call escribirsegundo_apellido
+
+
 
 	;Terminando el programa
 	call esperartecla
@@ -47,70 +59,79 @@ org	100h
 	;Aqui van todas las declaraciones de variables
 	section	.data
 
-	primernombre DB "Cesar$"
-	segundonombre DB "Eduardo$"
-	primerapellido DB "Esperanza$"
-	segundoapellido DB "Ulloa$"
+	primer_nombre DB "Cesar$"
+	segundo_nombre DB "Eduardo$"
+	primer_apellido DB "Esperanza$"
+	segundo_apellido DB "Ulloa$"
 
 
 	;Aqui van todas las funciones
 
 modotexto: 
-        MOV AH, 0h ; activa modo texto
-        MOV AL, 03h ; modo gráfico deseado, 03h denota un tamaño de 80x25 (80 columnas, 25 filas)
+        MOV AH, 0h ; activa el modo video/texto
+        MOV AL, 03h ; modo gráfico deseado, 03h denota un tamaño de pantalla de 80x25 (80 columnas, 25 filas)
         INT 10h
         RET
 
 movercursor1:
-        MOV AH, 02h ; posiciona el cursor en pantalla.
-	MOV DH, 10 ;fila en la que se mostrará el cursor
-        MOV DL, 20 ;columna en la que se mostrará el cursor
-        MOV BH, 0h ; número de página
+        MOV AH, 02h ; esto establece que queremos posicionar el cursor en pantalla.
+	MOV DH, 10 ; fila en la que se mostrará el cursor
+        MOV DL, 20 ; columna en la que se mostrará el cursor
+        MOV BH, 0h ; número de página del cursor
         INT 10h
         RET
 
 movercursor2:
-        MOV AH, 02h ; posiciona el cursor en pantalla.
-	MOV DH, 12 ;fila en la que se mostrará el cursor
-        MOV DL, 22 ;columna en la que se mostrará el cursor
-        MOV BH, 1h ; número de página
+        MOV AH, 02h ; esto establece que queremos posicionar el cursor en pantalla.
+	MOV DH, 12 ; fila en la que se mostrará el cursor
+        MOV DL, 20 ; columna en la que se mostrará el cursor
+        MOV BH, 0h ; número de página del cursor
         INT 10h
         RET
 
 movercursor3:
-        MOV AH, 02h ; posiciona el cursor en pantalla.
-	MOV DH, 14 ;fila en la que se mostrará el cursor
-        MOV DL, 24 ;columna en la que se mostrará el cursor
-        MOV BH, 2h ; número de página
+        MOV AH, 02h ; esto establece que queremos posicionar el cursor en pantalla.
+	MOV DH, 14 ; fila en la que se mostrará el cursor
+        MOV DL, 20 ; columna en la que se mostrará el cursor
+        MOV BH, 0h ; número de página del cursor
         INT 10h
         RET
 
-escribirprimernombre: ;utilizando interrupcion DOS
-        MOV AH, 09h ; escribe cadena en pantalla según posición del cursor
-        MOV DX, primernombre ; cadena que vamos a imprimir, debe ir sin corchetes
+movercursor4:
+        MOV AH, 02h ; esto establece que queremos posicionar el cursor en pantalla.
+	MOV DH, 16 ; fila en la que se mostrará el cursor
+        MOV DL, 20 ; columna en la que se mostrará el cursor
+        MOV BH, 0h ; número de página del cursor
+        INT 10h
+        RET
+
+
+escribirprimer_nombre: ;utilizando interrupcion DOS
+        MOV AH, 09h ; esto establece que queremos que se escriba una cadena en pantalla según posición del cursor
+        MOV DX, primer_nombre ; cadena que vamos a imprimir, debe ir sin corchetes
         INT 21h
         RET
 
-escribirsegundonombre: ;utilizando interrupcion DOS
-        MOV AH, 09h ; escribe cadena en pantalla según posición del cursor
-        MOV DX, segundonombre ; cadena que vamos a imprimir, debe ir sin corchetes
+escribirsegundo_nombre: ;utilizando interrupcion DOS
+        MOV AH, 09h ; esto establece que queremos que se escriba una cadena en pantalla según posición del cursor
+        MOV DX, segundo_nombre ; cadena que vamos a imprimir, debe ir sin corchetes
         INT 21h
         RET
 
-escribirprimerapellido: ;utilizando interrupcion DOS
-        MOV AH, 09h ; escribe cadena en pantalla según posición del cursor
-        MOV DX, primerapellido ; cadena que vamos a imprimir, debe ir sin corchetes
+escribirprimer_apellido: ;utilizando interrupcion DOS
+        MOV AH, 09h ; esto establece que queremos que se escriba una cadena en pantalla según posición del cursor
+        MOV DX, primer_apellido ; cadena que vamos a imprimir, debe ir sin corchetes
         INT 21h
         RET
 
-escribirsegundoapellido: ;utilizando interrupcion DOS
-        MOV AH, 09h ; escribe cadena en pantalla según posición del cursor
-        MOV DX, segundoapellido ; cadena que vamos a imprimir, debe ir sin corchetes
+escribirsegundo_apellido: ;utilizando interrupcion DOS
+        MOV AH, 09h ; esto establece que queremos que se escriba una cadena en pantalla según posición del cursor
+        MOV DX, segundo_apellido ; cadena que vamos a imprimir, debe ir sin corchetes
         INT 21h
         RET
 
 esperartecla:
-        MOV AH, 00h ; espera buffer del teclado para avanzar en la siguiente instrucción
+        MOV AH, 00h ; esto establece que se espera buffer del teclado para avanzar en la siguiente instrucción
         INT 16h
 
         ret
